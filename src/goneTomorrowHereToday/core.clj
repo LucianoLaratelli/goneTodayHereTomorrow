ac564205eb006366dcdea9eda9ff77442a22366d
Luciano Laratelli <luciano@laratelli.com>
2021-03-18 14:05:43 -0400

    first commit

---
+++ src/goneTomorrowHereToday/core.clj
@@ -0,0 +1,50 @@
+(ns goneTomorrowHereToday.core
+  (:require [clojure.edn :as edn]
+            [clojure.core.async :refer [chan close!]]
+            [discljord.messaging :as discord-rest]
+            [discljord.connections :as discord-ws]
+            [discljord.formatting :refer [mention-user]]
+            [discljord.events :refer [message-pump!]]))
+
+(def state (atom nil))
+
+(def bot-id (atom nil))
+
+(def config (edn/read-string (slurp "config.edn")))
+
+(defmulti handle-event (fn [type _data] type))
+
+(defn random-response [user]
+  (str (rand-nth (:responses config)) ", " (mention-user user) \!))
+
+(defmethod handle-event :message-create
+  [_ {:keys [channel-id author mentions] :as _data}]
+  (when (some #{@bot-id} (map :id mentions))
+    (discord-rest/create-message! (:rest @state) channel-id :content (random-response author))))
+
+(defmethod handle-event :ready
+  [_ _]
+  (discord-ws/status-update! (:gateway @state) :activity (discord-ws/create-activity :name (:playing config))))
+
+(defmethod handle-event :default [_ _])
+
+(defn start-bot! [token & intents]
+  (let [event-channel (chan 100)
+        gateway-connection (discord-ws/connect-bot! token event-channel :intents (set intents))
+        rest-connection (discord-rest/start-connection! token)]
+    {:events  event-channel
+     :gateway gateway-connection
+     :rest    rest-connection}))
+
+(defn stop-bot! [{:keys [rest gateway events] :as _state}]
+  (discord-rest/stop-connection! rest)
+  (discord-ws/disconnect-bot! gateway)
+  (close! events))
+
+(defn -main [& args]
+  (reset! state (start-bot! (:token config) :guild-messages))
+  (reset! bot-id (:id @(discord-rest/get-current-user! (:rest @state))))
+  (try
+    (message-pump! (:events @state) handle-event)
+    (finally (stop-bot! @state))))
+
